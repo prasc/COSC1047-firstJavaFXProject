3b646b7430a503dec460712925642cb2e0fe7c0f
prashathc <prashathc@gmail.com>
2020-10-19 09:43:17 -0400

    initial

---
+++ src/sample/Main.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btOk = new Button("OK");
+        Scene scene = new Scene(btOk, 200, 250);
+        primaryStage.setTitle("MyJavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
